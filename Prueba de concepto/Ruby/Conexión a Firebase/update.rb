5c948287ba668fb7e0a6499c6bfea72ca77f9033
dfonseca97 <dfonseca@eafit.edu.co>
2017-04-14 13:58:58 -0500

    Corrección en el update
    Corrección de consistencia.

--- Prueba de concepto/Ruby/Conexión a Firebase/update.rb
+++ Prueba de concepto/Ruby/Conexión a Firebase/update.rb
@@ -7,6 +7,6 @@ uri = 'https://prueba-2a1c3.firebaseio.com/'
 
 firebase = Firebase::Client.new(uri)
 
-firebase.update('https://prueba-2a1c3.firebaseio.com/prueba/-Kg1c7RiViMz1XTTnA_F/' , {:numero => '4'})
+firebase.update('https://prueba-2a1c3.firebaseio.com/users/-KhhojbnXRIUF_PnQkfa' , {:age => '20'})
 
 
