b7d284f764e9ffc78ea392b35344ea571abc44fc
Juanesfon5 <juanesfon55@hotmail.com>
2017-05-18 14:33:22 -0500

    Arreglo geolocalización Colombia

--- Raspberry/geolocation.py
+++ Raspberry/geolocation.py
@@ -3,9 +3,10 @@ import requests,simlocator,json
 def getLocation():
     (lac, cellTower,operator) = simlocator.getCellTowerInfo()
     #print lac, cellTower, operator
-    codesDict =  {'Airtel':[404,93],'Idea':[404,78]} # replace this with codes in your location. Refer https://en.wikipedia.org/wiki/Mobile_country_code
+    codesDict =  {'movistar':[732,001],'Tigo':[732,020],'Claro':[732,101]} # replace this with codes in your location. Refer https://en.wikipedia.org/wiki/Mobile_country_code
     mcc = codesDict[operator][0]
     mnc = codesDict[operator][1]
+    mnc = codesDict[operator][2]
     payload = {'considerIp':'false','cellTowers':[{'cellId':cellTower,'locationAreaCode':lac,'mobileCountryCode':mcc,'mobileNetworkCode':mnc}]}
     jsonPayload = json.dumps(payload)
     #print jsonPayload
